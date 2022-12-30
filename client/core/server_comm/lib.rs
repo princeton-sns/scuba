7b0fc4f0a8dad16d82c5e7a78d124965cb46b200
Natalie Popescu <nataliepopescu@gmail.com>
2022-12-29 21:05:00 -0500

    apply amit suggested changes and add a (currently-hanging) test

--- client/core/server_comm/lib.rs
+++ client/core/server_comm/lib.rs
@@ -1,119 +1,56 @@
 #![feature(async_closure)]
 
 //use crate::crypto::OlmWrapper;
-use std::collections::HashMap;
+use std::pin::Pin;
 use url::Url;
-use eventsource_client::Client;
-use eventsource_client::ClientBuilder;
-use eventsource_client::SSE;
+use eventsource_client::{Client, ClientBuilder, SSE};
 use urlencoding::encode;
-use futures::TryStreamExt;
-use reqwest::Result;
-use reqwest::Response;
-//use async_std::task;
+use futures::{Stream, task::{Context, Poll}};
+use reqwest::{Result, Response};
+use serde::Serialize;
 
 const IP_ADDR    : &str = "localhost";
 const PORT_NUM   : &str = "8080";
 const HTTP_PREFIX: &str = "http://";
 const COLON      : &str = ":";
 
-#[derive(Debug)]
 struct ServerComm<'a> {
-  ip_addr : &'a str,
-  port_num: &'a str,
   base_url: Url,
   //crypto  : &'a OlmWrapper,
   idkey   : &'a str,
   client  : reqwest::Client,
+  listener: Pin<Box<dyn Stream<Item = eventsource_client::Result<SSE>>>>,
   //event emitter
-  init_success: bool,
 }
 
 impl<'a> ServerComm<'a> {
-  fn new(
+  pub fn new(
     ip_arg: Option<&'a str>,
     port_arg: Option<&'a str>,
     //crypto: &'a OlmWrapper,
-    // emitter
-    idkey_arg: &'a str,
+    //emitter
+    idkey: &'a str,
   ) -> Self {
     let ip_addr = ip_arg.unwrap_or(IP_ADDR);
     let port_num = port_arg.unwrap_or(PORT_NUM);
+    let base_url = Url::parse(&vec![HTTP_PREFIX, ip_addr, COLON, port_num].join(""))
+        .expect("Failed base_url construction");
+    let listener = Box::new(
+        ClientBuilder::for_url(base_url.join("/events").expect("Failed join of /events").as_str()).expect("Failed in ClientBuilder::for_url")
+            .header("Authorization", &vec!["Bearer", idkey].join(" ")).expect("Failed header construction")
+            .build()
+        )
+        .stream();
     Self {
-      ip_addr : ip_addr,
-      port_num: port_num,
-      base_url: Url::parse(&vec![HTTP_PREFIX, ip_addr, COLON, port_num].join("")).expect(""),
+      base_url,
       //crypto: crypto,
-      idkey   : idkey_arg, //crypto.get_idkey(),
+      idkey, //: crypto.get_idkey(),
       client  : reqwest::Client::new(),
-      init_success: false,
+      listener,
     }
   }
 
-  async fn init(
-    ip_arg: Option<&'a str>,
-    port_arg: Option<&'a str>,
-    //crypto: &'a OlmWrapper,
-    // emitter
-    idkey_arg: &'a str,
-  ) -> Result<ServerComm<'a>> {
-    let sc = ServerComm::new(ip_arg, port_arg, idkey_arg);
-    let listener = ClientBuilder::for_url(sc.base_url.join("/events").expect("").as_str()).expect("")
-        .header("Authorization", &vec!["Bearer", sc.idkey].join(" ")).expect("")
-        .build();
-
-    let mut stream = listener.stream()
-        .map_ok(async move |event| match &event {
-          SSE::Comment(comment) => println!("Got comment: {:?}", comment),
-          SSE::Event(event) => {
-            println!("Got event: {:?}", event);
-            // FIXME
-            let base_url = Url::parse(&vec![HTTP_PREFIX, sc.ip_addr, COLON, sc.port_num].join("")).expect("");
-            let client = reqwest::Client::new();
-            match event.event_type.as_str() {
-              "otkey" => {
-                match client.post(base_url.join("/self/otkeys").expect("").as_str())
-                    .header("Content-Type", "application/json")
-                    .header("Authorization", vec!["Bearer", sc.idkey].join(" "))
-                    .json("more otkeys")
-                    .send()
-                    .await {
-                  // TODO turn into JSON and get 'otkey' field
-                  Ok(response) => println!("{:?}", response),
-                  Err(e) => println!("{:?}", e),
-                }
-              },
-              "msg" => {
-                let msg = &event.data; // TODO from JSON
-                println!("{:?}", msg);
-                // TODO emit msg to core
-                match client.delete(base_url.join("/self/messages").expect("").as_str())
-                    .header("Content-Type", "application/json")
-                    .header("Authorization", vec!["Bearer", sc.idkey].join(" "))
-                    .json(&msg) // TODO .seqID and to JSON
-                    .send()
-                    .await {
-                  Ok(response) => println!("{:?}", response),
-                  Err(e) => println!("{:?}", e),
-                }
-              },
-              &_ => println!("Unexpected event"),
-            }
-          },
-        })
-        .map_err(|e| println!("Error streaming events: {:?}", e));
-
-    //let stream_task = task::spawn(async move {
-    while let Ok(Some(_)) = stream.try_next().await {
-      println!("waiting...");
-    }
-    //});
-    //task::block_on(stream_task);
-
-    Ok(sc)
-  }
-
-  async fn send_message(&self, batch: HashMap::<&'a str, &'a str>) -> Result<Response> {
+  pub async fn send_message(&self, batch: Batch<'a>) -> Result<Response> {
     self.client.post(self.base_url.join("/message").expect("").as_str())
         .header("Content-Type", "application/json")
         .header("Authorization", vec!["Bearer", self.idkey].join(" "))
@@ -122,65 +59,128 @@ impl<'a> ServerComm<'a> {
         .await
   }
 
-  async fn get_otkey_from_server(&self, idkey: &'a str) -> Result<Response> {
+  pub async fn get_otkey_from_server(&self, idkey: &'a str) -> Result<Response> {
     let mut url = self.base_url.join("/devices/otkey").expect("");
     url.set_query(Some(&vec!["device_id", &encode(idkey)].join("=")));
     self.client.get(url.as_str())
         .send()
-        // TODO as JSON
         .await
-    //  Ok(response) => println!("{:?}", response), // TODO return otkey
-    //  Err(e) => println!("{:?}", e),
-    //}
+        //.json()
+        //.await
+        // TODO return otkey
+  }
+
+  async fn delete_messages_from_server(&self, msg: String) -> Result<Response> {
+    self.client.delete(self.base_url.join("/self/messages").expect("").as_str())
+        .header("Content-Type", "application/json")
+        .header("Authorization", vec!["Bearer", self.idkey].join(" "))
+        .body(msg) // TODO .seqID
+        .send()
+        .await
+  }
+
+  async fn add_otkeys_to_server(&self) -> Result<Response> {
+    self.client.post(self.base_url.join("/self/otkeys").expect("").as_str())
+        .header("Content-Type", "application/json")
+        .header("Authorization", vec!["Bearer", self.idkey].join(" "))
+        .json("more otkeys") // FIXME
+        .send()
+        .await
+  }
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Event {
+  Otkey,
+  Msg(String),
+}
+
+impl<'a> Stream for ServerComm<'a> {
+  type Item = eventsource_client::Result<Event>;
+
+  fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+    let event = self.listener.as_mut().poll_next(cx);
+    match event {
+      Poll::Pending => Poll::Pending,
+      Poll::Ready(None) => Poll::Pending,
+      Poll::Ready(Some(Err(err))) => Poll::Ready(Some(Err(err))),
+      Poll::Ready(Some(Ok(event))) => match event {
+        SSE::Comment(_) => Poll::Pending,
+        SSE::Event(event) => {
+          match event.event_type.as_str() {
+            "otkey" => Poll::Ready(Some(Ok(Event::Otkey))),
+            "msg" => {
+              let msg = String::from(event.data); // TODO from JSON
+              println!("Received msg: {:?}", msg);
+              Poll::Ready(Some(Ok(Event::Msg(msg))))
+            },
+            _ => Poll::Pending,
+          }
+        }
+      }
+    }
+  }
+}
+
+#[derive(Debug, Serialize)]
+pub struct Batch<'a> {
+  messages: Vec<Message<'a>>,
+}
+
+impl<'a> Batch<'a> {
+  pub fn new(messages: Vec<Message<'a>>) -> Self {
+    Self { messages }
+  }
+}
+
+#[derive(Debug, Serialize)]
+pub struct Message<'a> {
+  device_id: &'a str,
+  payload: &'a str,
+}
+
+impl<'a> Message<'a> {
+  pub fn new(device_id: &'a str, payload: &'a str) -> Self {
+    Self {
+      device_id,
+      payload,
+    }
   }
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::ServerComm;
-  use std::collections::HashMap;
+  use crate::{Event, ServerComm, Batch, Message};
+  use futures::TryStreamExt;
 
   #[tokio::test]
   async fn test_init_default() {
-    match ServerComm::init(None, None, "abcd").await {
-      Ok(server_comm) => {
-        assert_eq!(server_comm.init_success, true);
-      },
-      Err(e) => println!("{:?}", e),
-    }
+    assert_eq!(ServerComm::new(None, None, "abcd").try_next().await, Ok(Some(Event::Otkey)));
   }
 
   #[tokio::test]
   async fn test_send_simple() {
-    let mut batch = HashMap::<&str, &str>::new();
-    let payload = "hello from <abcd>";
-    batch.insert("abcd", payload);
-    match ServerComm::init(None, None, "abcd").await {
-      Ok(server_comm) => {
-        match server_comm.send_message(batch).await {
-          Ok(res) => println!("OK: {:?}", res),
-          Err(e) => println!("ERR: {:?}", e),
-        }
-      },
-      Err(e) => println!("{:?}", e),
-    }
-  }
-
-  #[tokio::test]
-  async fn test_get_otkey() {
-    match ServerComm::init(None, None, "abcd").await {
-      Ok(sc1) => {
-        match ServerComm::init(None, None, "efgh").await {
-          Ok(sc2) => {
-            match sc2.get_otkey_from_server(sc1.idkey).await {
-              Ok(res) => println!("OK: {:?}", res),
-              Err(e) => println!("ERR: {:?}", e),
-            }
+    let idkey = "abcd";
+    let payload = "hello";
+    let batch = Batch::new(vec![Message::new(idkey, payload)]);
+    println!("batch: {:?}", batch);
+    let mut server_comm = ServerComm::new(None, None, idkey);
+    assert_eq!(server_comm.try_next().await, Ok(Some(Event::Otkey)));
+    match server_comm.send_message(batch).await {
+      Ok(res) => {
+        println!("Send succeeded: {:?}", res);
+        match server_comm.try_next().await {
+          Ok(Some(Event::Msg(msg))) => {
+            println!("SUCCESS got msg event");
+            println!("msg: {:?}", msg);
+            assert_eq!(msg, payload);
           },
-          Err(e2) => println!("e2: {:?}", e2),
+          Ok(Some(Event::Otkey)) => println!("FAIL got otkey event"),
+          Ok(None) => println!("FAIL got none"),
+          Err(err) => println!("FAIL got error: {:?}", err),
         }
       },
-      Err(e1) => println!("e1: {:?}", e1),
+      Err(err) => println!("Send failed: {:?}", err),
     }
   }
 }

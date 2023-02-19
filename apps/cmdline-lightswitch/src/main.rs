274f8bb44a475a2a4adcee527c40aaa72a383aa5
Natalie Popescu <nataliepopescu@gmail.com>
2023-02-19 17:49:42 -0500

    add colors to printed text

--- apps/cmdline-lightswitch/src/main.rs
+++ apps/cmdline-lightswitch/src/main.rs
@@ -6,6 +6,19 @@ use reedline_repl_rs::Repl;
 use reedline_repl_rs::Result as ReplResult;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
+use yansi::Paint;
+
+fn paint_red_bold(input: &str) -> String {
+    Box::new(Paint::red(input).bold()).to_string()
+}
+
+fn paint_cyan_bold(input: &str) -> String {
+    Box::new(Paint::cyan(input).bold()).to_string()
+}
+
+fn paint_white_bold(input: &str) -> String {
+    Box::new(Paint::white(input).bold()).to_string()
+}
 
 #[derive(Clone)]
 struct LightswitchApp {
@@ -32,20 +45,21 @@ impl Operation {
 #[async_trait]
 impl CoreClient for LightswitchApp {
     async fn client_callback(&self, sender: String, message: String) {
+        println!("{}", paint_white_bold(format!("FROM: {}", sender).as_str()));
         match Operation::from_string(message) {
             Operation::On => {
                 if *self.light.read() {
-                    println!("Light is already on");
+                    println!("{}", paint_red_bold("Light is already on"));
                 } else {
-                    println!("Turning light on");
+                    println!("{}", paint_cyan_bold("Turning light on"));
                     *self.light.write() = true;
                 }
             }
             Operation::Off => {
                 if !*self.light.read() {
-                    println!("Light is already off");
+                    println!("{}", paint_red_bold("Light is already off"));
                 } else {
-                    println!("Turning light on");
+                    println!("{}", paint_cyan_bold("Turning light off"));
                     *self.light.write() = false;
                 }
             }
@@ -107,11 +121,17 @@ impl LightswitchApp {
 async fn main() -> ReplResult<()> {
     let app = Arc::new(LightswitchApp::new().await);
 
-    // TODO print out own idkey
-    let mut repl = Repl::new(app)
+    let mut repl = Repl::new(app.clone())
         .with_name("Lightswitch App")
         .with_version("v0.1.0")
         .with_description("Noise lightswitch app")
+        .with_banner(
+            paint_white_bold(
+                format!("IDKEY: {}", app.core.as_ref().unwrap().idkey())
+                    .as_str(),
+            )
+            .as_str(),
+        )
         .with_command_async(Command::new("on"), |_, context| {
             Box::pin(LightswitchApp::on(context))
         })

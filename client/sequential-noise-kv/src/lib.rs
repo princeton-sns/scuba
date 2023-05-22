7a3da38a61f4831aef934199d7248262ae755b08
Natalie Popescu <nataliepopescu@gmail.com>
2023-05-22 16:19:58 -0400

    doesn't compile due to nested dependency

--- client/sequential-noise-kv/src/lib.rs
+++ client/sequential-noise-kv/src/lib.rs
@@ -5,3 +5,18 @@ pub mod client;
 pub mod data;
 pub mod devices;
 pub mod metadata;
+
+use jni::objects::{JClass, JString};
+use jni::sys::jstring;
+use jni::JNIEnv;
+
+#[no_mangle]
+pub extern "system" fn Java_com_example_noiseperiodtracker_MainActivity_createStandaloneDevice<
+    'local,
+>(
+    mut env: JNIEnv<'local>,
+    class: JClass<'local>,
+) { //-> jni::objects::JValueGen {
+     //let client_obj = client::NoiseKVClient::new(None, None, false,
+     // None, None).await; client_obj.create_standalone_device();
+}
